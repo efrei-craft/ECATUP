f35bdcb34cdfab27820cb772a93250fe4bb9ee4e
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-10-14 19:05:36 +0200

    Inital commit.

---
+++ src/main/java/fr/efreicraft/ecatup/Main.java
@@ -0,0 +1,16 @@
+package fr.efreicraft.ecatup;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Main extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
