966f6bd68872da3d88eba6d6e5458097083fbb4f
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-10-16 19:16:15 +0200

    Kick player if he doesn't have permission to access server

--- src/main/java/fr/efreicraft/ecatup/listeners/Join.java
+++ src/main/java/fr/efreicraft/ecatup/listeners/Join.java
@@ -10,6 +10,11 @@ public class Join implements Listener {
     LuckPerms LP = Main.LP;
     @EventHandler
     public void onJoin(org.bukkit.event.player.PlayerJoinEvent event) {
+        if (!event.getPlayer().hasPermission("server." + Main.config.getString("server_name"))) {
+            event.getPlayer().kick(Component.text("§cVous n'avez pas la permission d'accéder ce serveur !"));
+            event.joinMessage(null);
+            return;
+        }
         String prefix = LP.getUserManager().getUser(event.getPlayer().getUniqueId()).getCachedData().getMetaData().getPrefix().replaceAll("&", "§");
         event.getPlayer().displayName(Component.text(prefix + event.getPlayer().getName()));
         event.joinMessage(event.getPlayer().displayName().append(Component.text("§7 a rejoint le serveur !")));
