141e16db7d14cdc4620a201a93b7ade9809c8c54
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-10-18 23:40:24 +0200

    Voir description
    Le chat peut être copié sous clic (donne la perm, le nom et la date d'envoi) ET on peut envoyer des msg colorés avec '&' (sous permission) :)

--- src/main/java/fr/efreicraft/ecatup/listeners/Chat.java
+++ src/main/java/fr/efreicraft/ecatup/listeners/Chat.java
@@ -12,6 +12,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static fr.efreicraft.ecatup.utils.Msg.colorize;
 
 public class Chat implements Listener {
@@ -22,13 +25,19 @@ public class Chat implements Listener {
     public void onChat(AsyncChatEvent event) {
         event.setCancelled(true);
 
-        boolean coloriseText = LP.getUserManager().loadUser(event.getPlayer().getUniqueId()).join().getCachedData().getPermissionData().checkPermission("ecatup.chat.speakcolor").asBoolean();
+        boolean coloriseText = LP.getUserManager().loadUser(event.getPlayer().getUniqueId()).join().getCachedData().getPermissionData().checkPermission("ecatup.chat.color").asBoolean();
         Component msg = event.getPlayer().displayName()
                 .append(Component.text(ChatColor.GRAY + ": "))
                 .append(coloriseText ? Component.text(colorize((TextComponent) event.message())) : event.message());
+        final String WHAT_TIME_IS_IT = " @ {" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "}";
+
+        StringBuilder msgCopie = new StringBuilder( ( (TextComponent) (msg.append(Component.text(ChatColor.GRAY + ": ")).append(event.message())) ).content() );
+
+        // Les messages Minecraft font au max 255 caractères, c'est pas très grave de les parcourir plusieurs fois.
+        while (msgCopie.indexOf("§") != -1) msgCopie.delete(msgCopie.indexOf("§"), msgCopie.indexOf("§") + 2);
 
         Bukkit.broadcast(msg
                 .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Copier")))
-                .clickEvent(ClickEvent.copyToClipboard(((TextComponent) msg).content())));
+                .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent)event.message()).content() + WHAT_TIME_IS_IT)));
     }
 }
