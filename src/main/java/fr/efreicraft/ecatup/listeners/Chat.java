f571218e11bd6e522f8513d77941586bc35f0fbd
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-10-24 01:19:58 +0200

    fix du stupide copiage et préfixe de canal

--- src/main/java/fr/efreicraft/ecatup/listeners/Chat.java
+++ src/main/java/fr/efreicraft/ecatup/listeners/Chat.java
@@ -4,6 +4,7 @@ import fr.efreicraft.ecatup.Main;
 import fr.efreicraft.ecatup.PreferenceCache;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
@@ -30,30 +31,28 @@ public class Chat implements Listener {
         event.setCancelled(true);
 
         boolean coloriseText = LP.getUserManager().loadUser(event.getPlayer().getUniqueId()).join().getCachedData().getPermissionData().checkPermission("ecatup.chat.color").asBoolean();
-
         PreferenceCache.ChatChannel channelActuel = PreferenceCache.getChannel(event.getPlayer().getUniqueId());
 
-        Component msg = Component.text(channelActuel == PreferenceCache.ChatChannel.SERVER ? "" : ("["+channelActuel+"]")).color(NamedTextColor.YELLOW).append(event.getPlayer().displayName())
+        Component channelPrefix = Component.text(channelActuel == PreferenceCache.ChatChannel.SERVER ? "" : ("["+channelActuel+"] ")).color(NamedTextColor.YELLOW);
+        Component msg = event.getPlayer().displayName()
                 .append(Component.text(ChatColor.GRAY + ": "))
-                .append(coloriseText ? Component.text(colorize((TextComponent) event.message())) : event.message());
+                .append(coloriseText ? Component.text(colorize((TextComponent) event.message())) : event.message())
+                .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Copier")));
+
         final String WHAT_TIME_IS_IT = " @ {" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "}";
 
-        StringBuilder msgCopie = new StringBuilder( ( (TextComponent) (msg.append(Component.text(ChatColor.GRAY + ": ")).append(event.message())) ).content() );
+        StringBuilder msgCopie = new StringBuilder(new String("["+channelActuel.toString()+"] ")).append(((TextComponent) msg).content());
 
         // Les messages Minecraft font au max 255 caractères, c'est pas très grave de les parcourir plusieurs fois.
         while (msgCopie.indexOf("§") != -1) msgCopie.delete(msgCopie.indexOf("§"), msgCopie.indexOf("§") + 2);
 
-        msgCopie.insert(0, "["+channelActuel+"] ");
-
         switch (channelActuel) {
-            case SERVER -> Bukkit.broadcast(msg
-                    .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Copier")))
-                    .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent)event.message()).content() + WHAT_TIME_IS_IT)));
+            case SERVER -> Bukkit.broadcast(Component.join(JoinConfiguration.noSeparators(), channelPrefix, msg
+                    .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent)event.message()).content() + WHAT_TIME_IS_IT))));
             case TEAM -> {
                 if (event.getPlayer().getScoreboardTags().isEmpty()) {
-                    Bukkit.broadcast(msg
-                            .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Copier")))
-                            .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent)event.message()).content() + WHAT_TIME_IS_IT)));
+                    Bukkit.broadcast(Component.join(JoinConfiguration.noSeparators(),channelPrefix,msg
+                            .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent)event.message()).content() + WHAT_TIME_IS_IT))));
                 } else {
                     Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(event.getPlayer());
 
@@ -65,9 +64,8 @@ public class Chat implements Listener {
                     }
 
                     for (String playerName : team.getEntries()) {
-                        if (Bukkit.getPlayer(playerName) != null) Bukkit.getPlayer(playerName).sendMessage(msg
-                                .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Copier")))
-                                .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent) event.message()).content() + WHAT_TIME_IS_IT)));
+                        if (Bukkit.getPlayer(playerName) != null) Bukkit.getPlayer(playerName).sendMessage(Component.join(JoinConfiguration.noSeparators(),channelPrefix, msg
+                                .clickEvent(ClickEvent.copyToClipboard(msgCopie + ": " + ((TextComponent) event.message()).content() + WHAT_TIME_IS_IT))));
                     }
                 }
             }
