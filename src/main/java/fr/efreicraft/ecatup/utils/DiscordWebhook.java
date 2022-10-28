dcb4b1defe5bce5ab3fc7692bdaab8d57122968f
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-10-28 19:53:52 +0200

    opti imports inutiles

--- src/main/java/fr/efreicraft/ecatup/utils/DiscordWebhook.java
+++ src/main/java/fr/efreicraft/ecatup/utils/DiscordWebhook.java
@@ -1,16 +1,13 @@
 package fr.efreicraft.ecatup.utils;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.awt.Color;
+import java.awt.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Array;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Class used to execute Discord Webhooks with low effort
