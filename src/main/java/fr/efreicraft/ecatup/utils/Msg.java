4f65c207136770e28ebdc89169ab5d36bd7e3fd5
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-01 21:44:21 +0100

    fix stupide message LP qui colorize un texte null

--- src/main/java/fr/efreicraft/ecatup/utils/Msg.java
+++ src/main/java/fr/efreicraft/ecatup/utils/Msg.java
@@ -1,14 +1,22 @@
 package fr.efreicraft.ecatup.utils;
 
 import net.kyori.adventure.text.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+
+import java.util.Objects;
 
 public class Msg {
     public static String colorize(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+        return ChatColor.translateAlternateColorCodes('&', Objects.requireNonNullElse(message, "null"));
     }
 
     public static String colorize(TextComponent message) {
-        return ChatColor.translateAlternateColorCodes('&', message.content());
+        return ChatColor.translateAlternateColorCodes('&', Objects.requireNonNullElse(message.content(), nullText()));
+    }
+
+    private static String nullText() {
+        Bukkit.getLogger().warning("Tried to colorize a null text (LuckPerms is at it again...)");
+        return "null";
     }
 }
