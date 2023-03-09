b51040e86184461bb2137488c9132933dea3be33
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-03-09 03:19:11 +0100

    finition de la commande /pperms. Il y reste quelques TODO dedans

--- src/main/java/fr/efreicraft/ecatup/commands/permissions/Common.java
+++ src/main/java/fr/efreicraft/ecatup/commands/permissions/Common.java
@@ -1,30 +1,47 @@
 package fr.efreicraft.ecatup.commands.permissions;
 
+import fr.efreicraft.animus.models.PermGroupPlayer;
+import fr.efreicraft.ecatup.ECATUP;
 import fr.efreicraft.ecatup.utils.MessageUtils;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Common {
 
     public static void sendGroupHelp(CommandSender player) {
         MessageUtils.sendMessage(player, MessageUtils.ChatPrefix.PLUGIN, "&3&l======= AIDE =======");
 
-        LegacyComponentSerializer serializer = LegacyComponentSerializer.legacyAmpersand();
-        player.sendMessage(serializer.deserialize("  &8» &e/gperms groups &3Afficher les groupes"));
-        player.sendMessage(serializer.deserialize("  &8» &e/gperms <group> &3Afficher les infos sur <group>"));
-        player.sendMessage(serializer.deserialize("  &8» &e/gperms <group> +<perm> [expiresIn] [context] &3Ajoute une permission au joueur"));
-        player.sendMessage(serializer.deserialize("  &8» &e/gperms <group> setPrefix [newPrefix] &3Change ou annule le préfixe du groupe (entre ')"));
-        player.sendMessage(serializer.deserialize("  &8» &e/gperms <group> -<perm> &3Retire une permission au joueur"));
+        MessageUtils.sendMessage(player, "  &8» &e/gperms groups &3Afficher les groupes");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> &3Afficher les infos sur <group>");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> add <player> &3Ajoute <player> à <group>");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> remove <player> &3Supprime <player> de <group>");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> +<perm> [expiresIn] [context] &3Ajoute une permission au joueur");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> setPrefix [newPrefix] &3Change ou annule le préfixe du groupe (entre ')");
+        MessageUtils.sendMessage(player, "  &8» &e/gperms <group> -<perm> &3Retire une permission au joueur");
     }
 
     public static void sendPlayerHelp(CommandSender player) {
         MessageUtils.sendMessage(player, MessageUtils.ChatPrefix.PLUGIN, "&3&l======= AIDE =======");
 
-        LegacyComponentSerializer serializer = LegacyComponentSerializer.legacyAmpersand();
-        player.sendMessage(serializer.deserialize("  &8» &e/pperms <player> &3Afficher les groupes du joueur"));
-        player.sendMessage(serializer.deserialize("  &8» &e/pperms <player> +<perm> [expiresIn] [context] &3Ajoute une permission au joueur"));
-        player.sendMessage(serializer.deserialize("  &8» &e/pperms <player> setPrefix [newPrefix] &3Change ou réinitialise le préfixe du joueur (entre ')"));
-        player.sendMessage(serializer.deserialize("  &8» &e/pperms <player> -<perm> &3Retire une permission au joueur"));
+        MessageUtils.sendMessage(player, "  &8» &e/pperms <player> &3Afficher les groupes du joueur");
+        MessageUtils.sendMessage(player, "  &8» &e/pperms <player> +<perm> [expiresIn] [[context1],[context2],[context3] OR ALL] &3Ajoute une permission au joueur");
+        MessageUtils.sendMessage(player, "  &8» &e/pperms <player> setPrefix [newPrefix] &3Change ou réinitialise le préfixe du joueur (entre ')");
+        MessageUtils.sendMessage(player, "  &8» &e/pperms <player> -<perm> &3Retire une permission au joueur");
+    }
+
+    // TODO: ajouter des infos ?
+    public static void printPlayerInfoTo(CommandSender to, Player player) {
+        MessageUtils.sendMessage(to, MessageUtils.ChatPrefix.PLUGIN, "&3======= &r%s&l%s &3=======".formatted(player.isOnline() ? "&a" : "&4", player.getName()));
+        // ew.
+        MessageUtils.sendMessage(to, "  &8• Groupes : " + String.join(", ",
+                ECATUP.getInstance()
+                .getPlayerManager()
+                .getPlayer(player)
+                .getAnimusPlayer()
+                .getPermGroups()
+                        .stream().map(PermGroupPlayer::getName)
+                        .toList()
+        ));
     }
 
 }
--- src/main/java/fr/efreicraft/ecatup/commands/permissions/PlayersCom.java
+++ src/main/java/fr/efreicraft/ecatup/commands/permissions/PlayersCom.java
@@ -1,25 +1,196 @@
 package fr.efreicraft.ecatup.commands.permissions;
 
+import fr.efreicraft.animus.endpoints.PlayerService;
+import fr.efreicraft.animus.invoker.ApiException;
+import fr.efreicraft.animus.models.PermissionInput;
+import fr.efreicraft.ecatup.ECATUP;
+import fr.efreicraft.ecatup.utils.MessageUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.List;
 
 public class PlayersCom implements TabExecutor {
+
+    private static final List<String> NEVER_EXPIRE = List.of(
+            "null",
+            "never",
+            "-1",
+            "0",
+            "jamais"
+    );
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length < 1) {
+        if (args.length == 0) {
             Common.sendPlayerHelp(sender);
             return true;
         }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            MessageUtils.sendMessage(sender, MessageUtils.ChatPrefix.PLUGIN, "&cAucun joueur '" + args[0] + "' trouvé ! Serait-il déconnecté...?");
+            return true;
+        }
+
+        switch (args.length) {
+            // Info joueur
+            case 1 -> Common.printPlayerInfoTo(sender, target);
+
+            case 2 -> {
+                if (args[1].startsWith("+")) {
+                    String permission = args[1].substring(1);
+                    try {
+                        PlayerService.addPermissions(target.getUniqueId().toString(), List.of(new PermissionInput().name(permission)));
+                        ECATUP.getInstance().getPlayerManager().getPlayer(target).updatePermission(permission, true);
+                    } catch (ApiException e) {
+                        MessageUtils.sendMessage(sender, "&cPas pu atteindre la base de données. Vos changements n'ont pas été effectués");
+                        Bukkit.getLogger().severe("=== ERREUR API ===");
+                        Bukkit.getLogger().severe("%s a essayé d'ajouter la permission '%s' à %s".formatted(sender.getName(), permission, target.getName()));
+                        Bukkit.getLogger().severe("Message d'exception API : %s".formatted(e.getMessage()));
+                        Bukkit.getLogger().severe("Code de réponse API : %s".formatted(e.getCode()));
+                        return true;
+                    }
+
+                    // Message de succès
+                    MessageUtils.sendMessage(sender, "&8[&a+&8] &e%s &8-> &e%s".formatted(target.getName(), permission));
+                }
+                else if (args[1].startsWith("-")) {
+                    String permission = args[1].substring(1);
+                    try {
+                        PermissionInput permissionInput = new PermissionInput().name(permission);
+
+                        PlayerService.revokePermissions(target.getUniqueId().toString(), List.of(permissionInput));
+                        ECATUP.getInstance().getPlayerManager().getPlayer(target).updatePermission(permission, false);
+                        // TODO Permissions personnelles à ajouter.
+                        // ECATUP.getInstance().getPlayerManager().getPlayer(target).getAnimusPlayer().permissions;
+                    } catch (ApiException e) {
+                        MessageUtils.sendMessage(sender, "&cPas pu atteindre la base de données. Vos changements n'ont pas été effectués");
+                        Bukkit.getLogger().severe("=== ERREUR API ===");
+                        Bukkit.getLogger().severe("%s a essayé de révoquer la permission '%s' à %s".formatted(sender.getName(), permission, target.getName()));
+                        Bukkit.getLogger().severe("Message d'exception API : %s".formatted(e.getMessage()));
+                        Bukkit.getLogger().severe("Code de réponse API : %s".formatted(e.getCode()));
+                        return true;
+                    }
+
+                    // Message de succès
+                    MessageUtils.sendMessage(sender, "&8[&4-&8] &e%s &8-> &e%s".formatted(target.getName(), permission));
+                } else if (args[1].equalsIgnoreCase("setPrefix")) {
+                    //TODO: préfixes joueurs ajoutés ??
+                    MessageUtils.sendMessage(sender, "&cSoon...");
+                }
+                else {
+                    MessageUtils.sendMessage(sender, "&cMauvais argument : '%s'".formatted(args[1]));
+                }
+            }
+
+            // Ajout de préfixe OU ajout de perm avec contexte
+            case 3 -> {
+                if (args[1].equalsIgnoreCase("setPrefix")) {
+                    //TODO: préfixes joueurs ajoutés ??
+                } else if (args[1].charAt(0) == '+') {
+                    String permission = args[1].substring(1);
+                    Duration expiresIn;
+
+                    try {
+                        if (NEVER_EXPIRE.contains(args[2])) {
+                            expiresIn = null;
+                        } else
+                            expiresIn = Duration.parse(args[2]);
+
+                        PermissionInput permissionInput = new PermissionInput().name(permission).expires(String.valueOf(expiresIn));
+                        PlayerService.addPermissions(target.getUniqueId().toString(), List.of(permissionInput));
+                        ECATUP.getInstance().getPlayerManager().getPlayer(target).updatePermission(permission, true);
+                        // TODO Permissions personnelles à ajouter.
+                        // ECATUP.getInstance().getPlayerManager().getPlayer(target).getAnimusPlayer().permissions;
+                    } catch (ApiException e) {
+                        MessageUtils.sendMessage(sender, "&cPas pu atteindre la base de données. Vos changements n'ont pas été effectués");
+                        Bukkit.getLogger().severe("=== ERREUR API ===");
+                        Bukkit.getLogger().severe("%s a essayé d'ajouter la permission '%s' à %s".formatted(sender.getName(), permission, target.getName()));
+                        Bukkit.getLogger().severe("Message d'exception API : %s".formatted(e.getMessage()));
+                        Bukkit.getLogger().severe("Code de réponse API : %s".formatted(e.getCode()));
+                        return true;
+                    } catch (DateTimeParseException e) {
+                        MessageUtils.sendMessage(sender, "&cMauvaise durée donnée : '%s'".formatted(args[2]));
+                        return true;
+                    }
+
+                    // Message de succès
+                    MessageUtils.sendMessage(sender, "&8[&a+&8] &e%s &8-> &e%s".formatted(target.getName(), permission));
+                    if (expiresIn != null)
+                        MessageUtils.sendMessage(sender, "  &7&o(expire le %s)".formatted(Instant.now().plus(expiresIn).toString()));
+                } else {
+                    MessageUtils.sendMessage(sender, "&cTrop d'arguments !");
+                }
+            }
+
+            // Ajout de perm avec expiration et contexte
+            case 4 -> {
+                if (args[1].charAt(0) != '+') {
+                    MessageUtils.sendMessage(sender, "&cTrop d'arguments !");
+                    return true;
+                }
+                String permission = args[1].substring(1);
+                Duration expiresIn;
+                List<String> contexts = List.of(args[3].split("[,;/]"));
+                // ALL absorbe tout le monde
+                if (contexts.contains("ALL") || contexts.contains("all")) contexts = List.of("ALL");
+
+                try {
+                    if (NEVER_EXPIRE.contains(args[2])) {
+                        expiresIn = null;
+                    } else
+                        expiresIn = Duration.parse(args[2]);
+
+                    PermissionInput permissionInput = new PermissionInput().name(permission).expires(String.valueOf(expiresIn)).serverTypes(contexts);
+                    PlayerService.addPermissions(target.getUniqueId().toString(), List.of(permissionInput));
+                    ECATUP.getInstance().getPlayerManager().getPlayer(target).updatePermission(permission, true);
+                    // TODO Permissions personnelles à ajouter.
+                    // ECATUP.getInstance().getPlayerManager().getPlayer(target).getAnimusPlayer().permissions;
+                } catch (ApiException e) {
+                    MessageUtils.sendMessage(sender, "&cPas pu atteindre la base de données. Vos changements n'ont pas été effectués");
+                    Bukkit.getLogger().severe("=== ERREUR API ===");
+                    Bukkit.getLogger().severe("%s a essayé d'ajouter la permission '%s' à %s".formatted(sender.getName(), permission, target.getName()));
+                    Bukkit.getLogger().severe("Message d'exception API : %s".formatted(e.getMessage()));
+                    Bukkit.getLogger().severe("Code de réponse API : %s".formatted(e.getCode()));
+                    return true;
+                } catch (DateTimeParseException e) {
+                    MessageUtils.sendMessage(sender, "&cMauvaise durée donnée : '%s'".formatted(args[2]));
+                    return true;
+                }
+
+                // Message de succès
+                MessageUtils.sendMessage(sender, "&8[&a+&8] &e%s &8-> &e%s".formatted(target.getName(), permission));
+                if (expiresIn != null)
+                    MessageUtils.sendMessage(sender, "  &7&o(expire le %s)".formatted(Instant.now().plus(expiresIn).toString()));
+                MessageUtils.sendMessage(sender, "  &7&o(valable dans %s)".formatted(Arrays.toString(contexts.toArray())));
+            }
+            default -> Common.sendPlayerHelp(sender);
+        }
         return true;
     }
-
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 2) {
+            return List.of("+<perm>", "setPrefix", "-<perm>");
+        } else if (args.length == 3) {
+            if (!args[1].equalsIgnoreCase("setPrefix"))
+                return List.of("never");
+        } else if (args.length == 4) {
+            if (!args[1].equalsIgnoreCase("setPrefix"))
+                return List.of("ALL", "lobby", "mini");
+        }
         return null;
     }
+
 }
